081ecc0e3de159e65950d6921d536332abd1d85e
niko-2112 <davy.nikola@gmail.com>
2021-04-15 08:18:25 -0700

    gibrid class

--- include/db_driver_gibrid_faculties.h
+++ include/db_driver_gibrid_faculties.h
@@ -1,8 +1,63 @@
-//
-// Created by niko2112 on 4/15/21.
-//
+// Copyright 2021 Davydov Nikolay <davy.nikola@gmail.com>
+
 
 #ifndef HOMEWORK_1_DB_DRIVER_GIBRID_FACULTIES_H
 #define HOMEWORK_1_DB_DRIVER_GIBRID_FACULTIES_H
+#include <vector>
+#include <string>
+#include <iostream>
+
+#include <db_driver.h>
+#include "basic_faculties.h"
+#include "branch_faculties.h"
+#include "faculties.h"
+
+class DB_gibrid_faculties: public DB_driver{
+private:
+    std::vector<Faculties *> vec_gibrid_faculties;
+public:
+    DB_gibrid_faculties();
+
+    void open(const std::string &name_db);
+
+    void close();
+
+    void add_record(const Branch_faculties &record);
+
+    void add_record(const Basic_faculties &record);
+
+    void all_records(std::fstream &file);
+
+    void print_reports();
+
+    template<class Compare = std::less<>>
+    void sort(Compare cmp = Compare{}) {
+        std::fstream file(path_ + separator_ + name_open_db_ + separator_ + file_name1_,
+                          std::ios::trunc | std::ios::out);
+
+        std::vector<Branch_faculties> new_vec_branch_faculties(num_records);
+        num_records = 0;
+        file.write((char *) &num_records, sizeof(num_records));
+        file.close();
+
+        merge_sort(vec_branch_faculties.begin(), vec_branch_faculties.end(), new_vec_branch_faculties.begin(),
+                   cmp);
+
+        vec_branch_faculties.resize(0);
+        for (auto branch_faculties : new_vec_branch_faculties) {
+            add_record(branch_faculties);
+        }
+    }
+
+    std::pair<Basic_faculties, Branch_faculties> select_by_NUK(const std::string &NUK);
+
+    std::pair<Basic_faculties, Branch_faculties> select_by_num_teachers(const size_t num_teachers);
+
+    void delete_record(const std::string &name);
+
+    void edit_record(const std::string &name_old_record, const Branch_faculties &new_record);
+
+    void edit_record(const std::string &name_old_record, const Basic_faculties &new_record);
+};
 
 #endif //HOMEWORK_1_DB_DRIVER_GIBRID_FACULTIES_H
--- sources/db_driver_gibrid_faculties.cpp
+++ sources/db_driver_gibrid_faculties.cpp
@@ -1,4 +1,61 @@
-//
-// Created by niko2112 on 4/15/21.
-//
+#include "db_driver_gibrid_faculties.h"
+
+DB_gibrid_faculties::DB_gibrid_faculties(): DB_driver("Gibrid Faculties"){}
+
+
+void DB_gibrid_faculties::open(const std::string &name_db) {
+    if (fs::is_directory(path_ + separator_ + name_db)) {
+        name_open_db_ = name_db;
+        std::fstream file(path_ + separator_ + name_open_db_ + separator_ + file_name1_,
+                          std::ios::binary | std::ios::in);
+        file.read((char *) &num_records, sizeof(num_records));
+        vec_gibrid_faculties.resize(num_records);
+        all_records(file);
+        file.close();
+    }
+}
+
+
+void DB_gibrid_faculties::close(){
+    name_open_db_ = "";
+    num_records = 0;
+    for(size_t i = 0; i < num_records; ++i){
+        delete[] vec_gibrid_faculties[i];
+    }
+    vec_gibrid_faculties.resize(0);
+    vec_gibrid_faculties.shrink_to_fit();
+}
+
+
+//void DB_gibrid_faculties::add_record(const Branch_faculties &record);
+
+
+//void DB_gibrid_faculties::add_record(const Basic_faculties &record);
+
+
+void DB_gibrid_faculties::all_records(std::fstream &file){
+    for(auto &faculties: vec_gibrid_faculties){
+        std::string type_faculties;
+        std::getline(file, type_faculties, '\0');
+        if(type_faculties == "basic_faculties")
+    }
+}
+
+
+//void DB_gibrid_faculties::print_reports();
+
+
+//std::pair<Basic_faculties, Branch_faculties> DB_gibrid_faculties::select_by_NUK(const std::string &NUK);
+
+
+//std::pair<Basic_faculties, Branch_faculties> DB_gibrid_faculties::select_by_num_teachers(const size_t num_teachers);
+
+
+//void DB_gibrid_faculties::delete_record(const std::string &name);
+
+
+//void DB_gibrid_faculties::edit_record(const std::string &name_old_record, const Branch_faculties &new_record);
+
+
+//void DB_gibrid_faculties::edit_record(const std::string &name_old_record, const Basic_faculties &new_record);
 
